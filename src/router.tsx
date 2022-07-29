00ef318c2ddf278adebccc355cfa73c2e48be410
suz8a <suzbel97@gmail.com>
2022-07-29 12:38:09 -0600

    add routes

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Cards from "./layouts/cards";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/cards" element={<Cards />} />
+      <Route path="*" element={<Navigate to="/cards" />} />
+    </Routes>
+  );
+};
